a26e84a1fbf0fff99f5a8640240d0d0fcfd7651d
Dcrispim <diego.crispim@yahoo.com.br>
2019-08-05 10:52:40 -0300

    87-Aplly-Middleware(Redux-Promise)

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,42 +1,56 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 
-import {changeDescription} from './todoActions'
+import {changeDescription, search} from './todoActions'
 
 import Grid from '../template/Grid';
 import IconButton from '../template/IconButton';
 
-const TodoForm = props =>{
-    const keyHandler = (e) =>{
+class TodoForm extends Component{
+    constructor(props){
+        super(props)
+
+        this.keyHandler = this.keyHandler.bind(this)
+    }
+
+    keyHandler(e){
         if(e.key == 'Enter'){
-            e.shiftKey ? props.handleSearch() : props.handleAdd()
+            e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()
         }else if (e.key == 'Escape'){
-            props.handleClear()
+            this.props.handleClear()
         }
     }
 
-    return (
-                <div role='form' className='todoForm'>
-                    <Grid cols="12 9 10">
-                        <input id='description' className='form-control'
-                        placeholder='Adicione uma tarefa' 
-                        value={props.description} 
-                        onChange={props.changeDescription}
-                        onKeyUp={keyHandler}>
-                        </input>
-                    </Grid>
-                    <Grid cols='12 3 2'>
-                        <IconButton style='primary' icon='plus' onClick={props.handleAdd}/>
-                        <IconButton style='info' icon='search' onClick={props.handleSearch}/>
-                        <IconButton style='light' icon='close' onClick={props.handleClear}/>
-                    </Grid>
-                </div>
-            )
+    componentWillMount(){
+        console.log(this.props.search())
+        this.props.search()
+    }
+
+    render (){
+        return (
+            <div role='form' className='todoForm'>
+                <Grid cols="12 9 10">
+                    <input id='description' className='form-control'
+                    placeholder='Adicione uma tarefa' 
+                    value={this.props.description} 
+                    onChange={this.props.changeDescription}
+                    onKeyUp={this.keyHandler}>
+                    </input>
+                </Grid>
+                <Grid cols='12 3 2'>
+                    <IconButton style='primary' icon='plus' onClick={this.props.handleAdd}/>
+                    <IconButton style='info' icon='search' onClick={this.props.search}/>
+                    <IconButton style='light' icon='close' onClick={this.props.handleClear}/>
+                </Grid>
+            </div>
+        )
 }
+}
+
 
 const MapStateToProps = state => ({description: state.todo.description})
 const MapDispatchToProps = dispatch => 
-    bindActionCreators({changeDescription}, dispatch)
+    bindActionCreators({changeDescription, search}, dispatch)
 
 export default connect(MapStateToProps, MapDispatchToProps)(TodoForm)
--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,29 +1,13 @@
-const INITIAL_STATE = {
-    description: 'Ler Livro',
-    list: [
-        {
-            _id:1,
-            description:'Pagar Fatura',
-            done: true
-        },
-        {
-            _id:2,
-            description: 'Reunião',
-            done:false
-
-        },
-        {
-            _id:3,
-            description:'Consulta',
-            done: false
-        }
-    ]
-}
+const INITIAL_STATE = {description: '', list: []}
 
 export default (state = INITIAL_STATE, action) => {
     switch(action.type){
         case 'DESCRIPTION_CHANGED':
             return {...state, description: action.payload}
+
+        case 'TODO_SEARCHED':
+            return {...state, list: action.payload.data}
+        
         default:
             return state
     }
