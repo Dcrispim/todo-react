da92525e2978eb672d2f2756b9b5c5ea83352237
Dcrispim <diego.crispim@yahoo.com.br>
2019-08-05 00:22:24 -0300

    85-Configurando-o-Redux

---
+++ frontend/src/main/reducers.js
@@ -0,0 +1,29 @@
+import {combineReducers} from 'redux'
+
+const rootReducer = combineReducers(
+    {
+        todo: ()=>({
+            description: 'Ler Livro',
+            list: [
+                {
+                    _id:1,
+                    description:'Pagar Fatura',
+                    done: true
+                },
+                {
+                    _id:2,
+                    description: 'Reunião',
+                    done:false
+
+                },
+                {
+                    _id:3,
+                    description:'Consulta',
+                    done: false
+                }
+            ]
+        })
+    }
+)
+
+export default rootReducer
